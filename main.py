7a960ac138b82b4f2a47279665ce46ab98074dc4
Saima Yunus <saimayunus925@gmail.com>
2022-12-11 16:22:48 -0800

    added Python dictionary for mapping between English chars and Morse code

--- main.py
+++ main.py
@@ -3,6 +3,25 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def python_morse_code_dictionary() -> dict:
+    # returns Python 'English to Morse Code' dictionary
+    return {
+        'A': '.-', 'B': '-...', 'C': '-.-.',
+        'D': '-..', 'E': '.', 'F': '..-.',
+        'G': '--.', 'H': '....', 'I': '..',
+        'J': '.---', 'K': '-.-', 'L': '.-..',
+        'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.',
+        'S': '...', 'T': '-', 'U': '..-',
+        'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..',
+
+        '0': '-----', '1': '.----', '2': '..---',
+        '3': '...--', '4': '....-', '5': '.....',
+        '6': '-....', '7': '--...', '8': '---..',
+        '9': '----.'
+    }
+    # source for this dictionary: https://stackoverflow.com/questions/32094525/morse-code-to-english-python3
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
